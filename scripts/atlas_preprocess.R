e9b4073cb6212754e9c91729e99af52c6bc3614a
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2012-08-20 18:20:08 +0300

    Moified atlas script

--- scripts/atlas_preprocess.R
+++ scripts/atlas_preprocess.R
@@ -1,3 +1,6 @@
+atlas.data <- read.csv("C:/SuoZ/data/Lintuatlas3_12lajia/Lintuatlas3_12lajia.csv", 
+                       header=TRUE, sep=",")
+
 atlas.data.coord <- ddply(atlas.data, .(Finnish), summarise, 
                           wmn_x = weighted.mean(gri_e10, atlas_max),
                           wmn_y = weighted.mean(gri_n10, atlas_max))
@@ -12,7 +15,10 @@ atlas.points <- atlas2points(atlas.data.coord,
 
 finland <- readShapeSpatial("H:/Data/Finland/Finland_borders.shp",
                             proj4string=CRS("+init=epsg:2393"))
+
 p.pch <- c(2:length(atlas.points$Finnish))
+
 spl <- list('sp.points', atlas.points, pch=p.pch[atlas.points$Finnish], 
             cex=0.75, col='black')
+
 spplot(finland, "Suojelualu", sp.layout=spl, colorkey=FALSE)
