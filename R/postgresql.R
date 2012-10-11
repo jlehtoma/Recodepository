5a72e4819fbf1dbd18d022939a59b941f2ac5f32
Joona Lehtomaki <joona.lehtomaki@gmail.com>
2012-10-11 22:29:51 +0300

    Changes in postgresql connection

--- R/postgresql.R
+++ R/postgresql.R
@@ -29,16 +29,17 @@ fetch.rli.data <- function(what="*", table, rows) {
   return(fetch(rs, n=rows))
 }
 
-upload <- function(con, df, table.name, overwrite=FALSE) {
+upload <- function(con, df, table.name, overwrite=FALSE, row.names=FALSE, ...) {
   if (dbExistsTable(con, table.name)) {
     
     if (overwrite) {
       message(paste("Overwriting old table:", table.name))
-      return(dbWriteTable(con, table.name, df, overwrite=TRUE))
+      return(dbWriteTable(con, table.name, df, overwrite=TRUE, 
+                          row.names=row.names))
     } else {
       stop(paste("Table", table.name, "exists and overwrite is off."))
     }
   } else {
-    return(dbWriteTable(con, table.name, df))
+    return(dbWriteTable(con, table.name, df, row.names=row.names, ...))
   }
 } 
